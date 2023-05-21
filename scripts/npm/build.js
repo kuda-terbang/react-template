a94e057dcc0db00e7528a3b9c9fccb3b80cfa0e7
robbycp <robbycaesar@gmail.com>
2023-05-21 14:14:20 +0700

    feat[github-action]: [FIX] script release command

--- scripts/npm/build.js
+++ scripts/npm/build.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 const build = async () => {
   console.log('> build packages');
   execSync(
-    'nx run-many --target build --projects=generator-utils,core-design-generator,core-util-generator,core-util-web-generator,react-generator,nextjs-client-template,react-dashboard-template'
+    'npx nx run-many --target build --projects=generator-utils,core-design-generator,core-util-generator,core-util-web-generator,react-generator,nextjs-client-template,react-dashboard-template'
   );
 };
 
