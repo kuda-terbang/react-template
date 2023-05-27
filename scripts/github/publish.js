bc082c86598654226947cb3d5a0f4a3e1f824f00
robbycp <robbycaesar@gmail.com>
2023-05-27 21:38:37 +0700

    feat[github-action]: [MODIFY] when PR not found, create PR release

--- scripts/github/publish.js
+++ scripts/github/publish.js
@@ -1,13 +1,5 @@
 const {version} = require('../../package.json')
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
 
-module.exports = async () => {
-	try {
-    const { stdout, stderr } = await exec(`npm run release ${version} --loglevel=debug`);
-    console.log('stdout:', stdout);
-    console.error('stderr:', stderr);
-  } catch (error) {
-    console.error('Error occurred:', error);
-  }
+module.exports = async ({context, exec, github}) => {
+	await exec.exec(`npm run release ${version} --loglevel=debug`)
 }
