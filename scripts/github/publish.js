f708c78e84a25afeefa58ff059764bd1e3e55f21
robbycp <robbycaesar@gmail.com>
2023-05-27 21:02:31 +0700

    feat[github-action]: [MODIFY] upgrade github-script version

--- scripts/github/publish.js
+++ scripts/github/publish.js
@@ -1,5 +1,13 @@
 const {version} = require('../../package.json')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-module.exports = async ({context, exec, github}) => {
-	exec(`npm run release ${version} --loglevel=debug`)
+module.exports = async () => {
+	try {
+    const { stdout, stderr } = await exec(`npm run release ${version} --loglevel=debug`);
+    console.log('stdout:', stdout);
+    console.error('stderr:', stderr);
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
 }
